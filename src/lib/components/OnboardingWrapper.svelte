cd2aa08a7c50c6bc9a156e63cd47557c00c27f5c
dp <dietrich.poensgen@heg-portal.de>
2023-01-31 16:40:59 +0100

    Komponentenerstellung

---
+++ src/lib/components/OnboardingWrapper.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+</script>
+
+<div
+	in:fly={{ x: -50, duration: 250, delay: 300 }}
+	out:fly={{ x: -50, duration: 250 }}
+	class="m-6 p-3 bg-primary-500 shadow-xl shadow-primary-500/60  rounded-xl min-h-[90vh] grid grid-cols-1 place-items-center gap-4"
+>
+	<slot />
+</div>
---
+++ src/lib/components/TextWrapper.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let invisible = false;
+</script>
+
+<section class="prose-primary prose xl:prose-lg p-4" class:invisible={invisible}>
+    <slot />
+</section>
