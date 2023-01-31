26dc30ff96f71eaf78ad2f4da9a376ab9f29d004
dp <dietrich.poensgen@heg-portal.de>
2023-01-31 16:40:27 +0100

    Kompass Komponente

--- src/routes/template/kompass/+page.svelte
+++ src/routes/template/kompass/+page.svelte
@@ -1,14 +1,122 @@
+<script lang="ts">
+	import { Geolocation } from '@capacitor/geolocation';
+
+	let heg_coord = "52,95988° N, 10,56097° O"
+
+	let radius: number = 18;
+
+	let width = radius * 2 + 4;
+	let height = radius * 2 + 4;
+
+	let cx = width / 2;
+	let cy = height / 2;
+	let outerRadius = radius;
+
+	let innerRadius = 3;
+	let needleInnerRadius = 5;
+	let timer = null;
+	let northRotation = 300;
+	let rotation = 300;
+
+	function deg2rad(deg: number) {
+		return (deg * Math.PI) / 180;
+	}
+	
+	function arc(
+		rx: number,
+		ry: number,
+		xAxisRotation: number,
+		largeArcFlag: number,
+		sweepFlag: number,
+		x: number,
+		y: number
+	) {
+		return (
+			'A' +
+			rx +
+			',' +
+			ry +
+			',' +
+			xAxisRotation +
+			',' +
+			largeArcFlag +
+			',' +
+			sweepFlag +
+			',' +
+			x +
+			',' +
+			y
+		);
+	}
+	function move(x: number, y: number) {
+		return 'M' + x + ',' + y;
+	}
+	function line(x: number, y: number) {
+		return 'L' + x + ',' + y;
+	}
+	function ease(pos: number) {
+		// pos [0..1]
+		if (pos < 0.8) {
+			var t = pos / 0.8;
+			return t * t * t;
+		}
+		return 1 + Math.sin(((pos - 0.8) / 0.2) * Math.PI) * 0.1;
+	}
+
+	function needlePath() {
+		var angle = deg2rad(45);
+
+		// arc start
+		var sx = cx + Math.sin(angle) * needleInnerRadius;
+		var sy = cy - Math.cos(angle) * needleInnerRadius;
+
+		// arc end
+		var ex = sx;
+		var ey = cy + Math.cos(angle) * needleInnerRadius;
+
+		// tip of needle
+		var tx = cx + radius - 2;
+		var ty = cy;
+		return (
+			move(sx, sy) +
+			' ' +
+			arc(needleInnerRadius, needleInnerRadius, 0, 0, 1, ex, ey) +
+			' ' +
+			line(tx, ty)
+		);
+	}
+
+	function southNeedleTransform() {
+		return 'rotate(180,' + cx + ',' + cy + ')';
+	}
+</script>
+
 <section class="grid place-items-center">
-	<svg xmlns="http://www.w3.org/2000/svg" width="360" height="360" fill="none"
-		><path
-			stroke="#1E40AF"
-			stroke-miterlimit="10"
-			stroke-width="32"
-			d="M315 180c0-74.531-60.469-135-135-135S45 105.469 45 180s60.469 135 135 135 135-60.469 135-135Z"
-		/><path
-			fill="#B91C1C"
-			d="m246.565 106.123-82.406 32.962a44.994 44.994 0 0 0-25.074 25.074l-32.962 82.406a5.63 5.63 0 0 0 1.244 6.068 5.629 5.629 0 0 0 6.068 1.244l82.406-32.962a44.994 44.994 0 0 0 25.074-25.074l32.962-82.406a5.629 5.629 0 0 0-1.244-6.068 5.63 5.63 0 0 0-6.068-1.244ZM180 196.875a16.875 16.875 0 1 1 0-33.75 16.875 16.875 0 0 1 0 33.75Z"
-		/>
-		<circle width="32" height="32" fill="#B91C1C" />
+	<svg {width} {height}>
+		<circle {cx} {cy} r={outerRadius} class="logo-outer-circle" />
+		<circle {cx} {cy} r={innerRadius} class="logo-inner-circle" />
+		<g transform="transform">
+			<path d={needlePath()} class="logo-needle north" />
+			<path d={needlePath()} transform={southNeedleTransform()} class="logo-needle" />
+		</g>
 	</svg>
 </section>
+
+<style>
+	.logo-outer-circle {
+		stroke: #777;
+		stroke-width: 2px;
+		fill: none;
+	}
+	.logo-inner-circle {
+		fill: #777;
+		stroke: none;
+	}
+	.logo-needle {
+		fill: #777;
+		stroke: none;
+	}
+	.north {
+		fill: #f55;
+	}
+</style>
