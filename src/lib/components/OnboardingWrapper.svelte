5668a71c88d43c31e9743e819bbf1a4614f54a94
dp <dietrich.poensgen@heg-portal.de>
2023-02-05 13:11:20 +0100

    style adjustments

--- src/lib/components/OnboardingWrapper.svelte
+++ src/lib/components/OnboardingWrapper.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import { fly } from 'svelte/transition';
+
+	//TODO: implement that the delay is only implemented when passed down via props
+	export let delay = false;
 </script>
 
 <div
-	in:fly={{ x: -50, duration: 250, delay: 300 }}
-	out:fly={{ x: -50, duration: 250 }}
+
+	in:fly={{ x: 400, duration: 400, delay: 400 }}
+	out:fly={{ x: -400, duration: 400 }}
 	class="m-6 p-3 bg-primary-500 shadow-xl shadow-primary-500/60  rounded-xl min-h-[90vh] grid grid-cols-1 place-items-center gap-4"
 >
 	<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,18 +14,18 @@
 	<p>hier im Herzog-Ernst-Gymnasium. Bevor du startest, wähle bitte deinen Spielmodus aus:</p>
 </TextWrapper>
 
-<section class="grid grid-cols-2 m-auto max-w-md gap-3">
+<section class="grid grid-cols-2 m-auto max-w-md gap-3 px-4">
 	<a
 		href="/real/01-start/permissions"
 		on:click={() => showToast('Im Gebäude')}
-		class="p-2 rounded bg-primary-600 text-white hover:bg-white hover:text-primary-600 border border-primary-600"
+		class="p-2 rounded grid place-items-center bg-primary-600 text-white hover:bg-white hover:text-primary-600 border border-primary-600"
 	>
 		Im Gebäude
 	</a>
 	<a
 		href="/"
 		on:click={() => showToast('Von Zuhause')}
-		class="p-2 rounded bg-primary-600 text-white hover:bg-white hover:text-primary-600 border border-primary-600"
+		class="p-2 rounded grid place-items-center bg-slate-600 text-slate-50 italic"
 	>
 		Von Zuhause
 	</a>
