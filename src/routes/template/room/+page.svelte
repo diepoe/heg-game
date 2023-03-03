0b11996ed104710c5484164bf507b84f01888faa
Guenni1887 <finn.unger.mail@gmail.com>
2023-03-03 11:49:36 +0000

    Atrium erstentwurf

---
+++ src/routes/template/room/+page.svelte
@@ -0,0 +1,15 @@
+<img src="https://www.heg-uelzen.de/wp-content/uploads/2022/07/1-08.JPG" alt="Bild Atrium" />
+
+<div class="p-10 relative -top-8 bg-white rounded-xl space-y-2">
+	<heading class="prose">
+		<h2>Atrium</h2>
+	</heading>
+	<section class="bg-primary-600 rounded-lg  max-w-fit p-2 text-white text-sm">
+		Hauptgebäude
+	</section>
+	<p class="text-sm pt-10">
+		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, saepe! Inventore
+		consequuntur aperiam assumenda mollitia dolor ab suscipit numquam, eum, at quas et, odit culpa
+		laboriosam voluptatibus corporis tempore ut.
+	</p>
+</div>
