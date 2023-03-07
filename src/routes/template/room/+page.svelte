ab7f25ee5d664cf333b4549765701573b0f74ec2
Guenni1887 <finn.unger.mail@gmail.com>
2023-03-07 07:15:00 +0000

    botton

--- src/routes/template/room/+page.svelte
+++ src/routes/template/room/+page.svelte
@@ -1,6 +1,6 @@
 <img src="https://www.heg-uelzen.de/wp-content/uploads/2022/07/1-08.JPG" alt="Bild Atrium" />
 
-<div class="p-10 relative -top-8 bg-white rounded-xl space-y-2">
+<div class="p-10 relative -top-8 bg-white rounded-xl space-y-2 w-full">
 	<heading class="prose">
 		<h2>Atrium</h2>
 	</heading>
@@ -13,6 +13,14 @@
 		laboriosam voluptatibus corporis tempore ut.
 	</p>
 </div>
+
+	<a
+		href="#"
+		class="bg-primary-600 rounded-lg grid place-items-center mx-9 absolute bottom-8 w-[80%] p-2 text-white text-sm shadow-lg shadow-primary-500"
+	>
+		Zum nächsten Raum
+	</a>
+
 
 <style>
 	body {
