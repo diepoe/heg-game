fd20df4b9287eb97a157556f3275fb12c7ce0abd
Dietrich Poensgen <poedietrich@outlook.de>
2023-05-09 17:34:25 +0200

    fix typo

--- src/routes/real/02-game/d-win/+page.svelte
+++ src/routes/real/02-game/d-win/+page.svelte
@@ -7,7 +7,7 @@
 >
 	<TextWrapper inverted={true} className="prose-h1:text-primary-50 prose-a:text-primary-50">
 		<h1>Gratulation</h1>
-		<p>Du hast die gefahrvollen Unterrichtswelten überstaden und die Direktorin befreit!</p>
+		<p>Du hast die gefahrvollen Unterrichtswelten überstanden und die Direktorin befreit!</p>
         <p>Deiner heldenhaften Reise zu dank wurde der LeG-Orden vertrieben!</p>
 
 		<p><a href="/real/01-start"><b>Eine neue Runde starten</b></a></p>
